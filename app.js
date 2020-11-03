a2b80122e73fa5c60d0b36fa19de20776d517b32
cecil <migayicecil@gmail.com>
2020-11-03 16:34:38 +0300

    changelog updated

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ app.get('/', (req, res) => {
   res.render('home', {});
 });
 
+app.get('/user-home', (req, res) => {
+  res.render('user_home', {});
+});
+
 app.listen(3000, () => {
   console.log("Tellem running in port 3000");
 });
